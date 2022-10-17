f0fffbe6d03eeec413770dffa12d4ea23e8c88b4
Bahl33ted <bahl33ted@outlook.com>
2022-10-17 12:41:27 -0500

    PowerCLI script to reset admin password on ESX host
    Simple ESXCLI script to reset admin password

---
+++ PowerCLI-Reset-Admin-Password.ps1
@@ -0,0 +1,8 @@
+$esxcli = Get-EsxCli -VMhost ESX1.domain.net -V2
+$arguments = $esxcli.system.account.set.CreateArgs()
+$arguments.id = 'admin'
+$arguments.password = 'T0tal1y$ecure'
+$arguments.passwordconfirmation = 'T0tal1y$ecure'
+$arguments
+
+$esxcli.system.account.set.Invoke($arguments)
