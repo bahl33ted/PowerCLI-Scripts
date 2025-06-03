593d7e6f3a42ff1898a0a5e0023c88210a712102
Bahl33ted <bahl33ted@outlook.com>
2025-06-03 16:28:39 -0500

    Create PowerCLI-Get-VISession and Disconnect-VISession.ps1
    Functions

---
+++ PowerCLI-Get-VISession and Disconnect-VISession Functions.ps1
@@ -0,0 +1,67 @@
+Function Get-ViSession {
+<#
+.SYNOPSIS
+Lists vCenter Sessions.
+
+.DESCRIPTION
+Lists all connected vCenter Sessions.
+
+.EXAMPLE
+PS C:\> Get-VISession
+
+.EXAMPLE
+PS C:\> Get-VISession | Where { $_.IdleMinutes -gt 5 }
+#>
+$SessionMgr = Get-View $DefaultViserver.ExtensionData.Client.ServiceContent.SessionManager
+$AllSessions = @()
+$SessionMgr.SessionList | Foreach {
+$Session = New-Object -TypeName PSObject -Property @{
+Key = $_.Key
+UserName = $_.UserName
+FullName = $_.FullName
+LoginTime = ($_.LoginTime).ToLocalTime()
+LastActiveTime = ($_.LastActiveTime).ToLocalTime()
+
+}
+If ($_.Key -eq $SessionMgr.CurrentSession.Key) {
+$Session | Add-Member -MemberType NoteProperty -Name Status -Value “Current Session”
+} Else {
+$Session | Add-Member -MemberType NoteProperty -Name Status -Value “Idle”
+}
+$Session | Add-Member -MemberType NoteProperty -Name IdleMinutes -Value ([Math]::Round(((Get-Date) – ($_.LastActiveTime).ToLocalTime()).TotalMinutes))
+$AllSessions += $Session
+}
+$AllSessions
+}
+
+
+Function Disconnect-ViSession {
+<#
+.SYNOPSIS
+Disconnects a connected vCenter Session.
+
+.DESCRIPTION
+Disconnects a open connected vCenter Session.
+
+.PARAMETER  SessionList
+A session or a list of sessions to disconnect.
+
+.EXAMPLE
+PS C:\> Get-VISession | Where { $_.IdleMinutes -gt 5 } | Disconnect-ViSession
+
+.EXAMPLE
+PS C:\> Get-VISession | Where { $_.Username -eq “User19” } | Disconnect-ViSession
+#>
+[CmdletBinding()]
+Param (
+[Parameter(ValueFromPipeline=$true)]
+$SessionList
+)
+Process {
+$SessionMgr = Get-View $DefaultViserver.ExtensionData.Client.ServiceContent.SessionManager
+$SessionList | Foreach {
+Write “Disconnecting Session for $($_.Username) which has been active since $($_.LoginTime)”
+$SessionMgr.TerminateSession($_.Key)
+}
+}
+}
