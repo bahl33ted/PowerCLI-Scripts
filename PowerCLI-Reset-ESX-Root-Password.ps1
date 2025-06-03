d6c3b99b73f6f21d37ce9eeeb8ed6c6c7468e150
Bahl33ted <bahl33ted@outlook.com>
2025-06-03 16:32:04 -0500

    Update and rename PowerCLI-Reset-Admin-Password.ps1 to PowerCLI-Reset-ESX-Root-Password.ps1

--- PowerCLI-Reset-ESX-Root-Password.ps1
+++ PowerCLI-Reset-ESX-Root-Password.ps1
@@ -1,6 +1,6 @@
 $esxcli = Get-EsxCli -VMhost ESX1.domain.net -V2
 $arguments = $esxcli.system.account.set.CreateArgs()
-$arguments.id = 'admin'
+$arguments.id = 'root'
 $arguments.password = 'T0tal1y$ecure'
 $arguments.passwordconfirmation = 'T0tal1y$ecure'
 $arguments
